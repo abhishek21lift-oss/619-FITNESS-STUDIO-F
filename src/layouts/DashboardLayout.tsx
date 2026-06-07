a951a1b8d4cb39e26fee00021bef9b0e496265e8
unknown <abhishek21lift@gmail.com>
2026-06-07 01:29:06 -0700

    feat: auth guard on dashboard/trainer layouts

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,6 +1,8 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { getToken } from '../api'
 
 export default function DashboardLayout() {
+  if (!getToken()) return <Navigate to="/fitness-center/login" replace />
   return (
     <div className="min-h-screen bg-black text-white">
       <Outlet />
--- src/layouts/TrainerLayout.tsx
+++ src/layouts/TrainerLayout.tsx
@@ -1,6 +1,8 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
+import { getToken } from '../api'
 
 export default function TrainerLayout() {
+  if (!getToken()) return <Navigate to="/trainer-dashboard/login" replace />
   return (
     <div className="min-h-screen bg-black text-white">
       <Outlet />
